47b7c3854f18938c8ee2ddc0fee6ef0120f37078
hai-nguyen1112 <hainguyen871112@gmail.com>
2025-11-15 13:55:49 -0500

    Styled Error

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -5,9 +5,11 @@ function Error() {
   const error = useRouteError();
 
   return (
-    <div>
+    <div className="m-8">
       <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
+      <p className="mb-2 mt-2 w-fit rounded-md bg-red-100 p-2 text-xs text-red-700">
+        {error.data || error.message}
+      </p>
       <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
