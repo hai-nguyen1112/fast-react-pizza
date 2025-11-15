2a40e72898302a5940d1ecb778cd676882e9c618
hai-nguyen1112 <hainguyen871112@gmail.com>
2025-11-15 10:44:54 -0500

    Removed console.log

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable no-unused-vars */
 import { useFetcher } from 'react-router-dom';
 import Button from '../../ui/Button';
 
 export default function UpdateOrder({ order }) {
   const fetcher = useFetcher();
-  console.log(order);
-  console.log(fetcher);
+
   return (
     <fetcher.Form method="PATCH" className="text-right">
       <Button type="primary">Make priority</Button>
